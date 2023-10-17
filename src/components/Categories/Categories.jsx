41dbae857f060966709ad88dc05519317708b9db
VS <vvs.jkr@gmail.com>
2023-10-17 12:27:28 +0300

    refactoring

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -3,7 +3,25 @@ import styles from "./styles.module.css";
 const Categories = ({ categories, setSelectedCategory, selectedCategory }) => {
   return (
     <div className={styles.categories}>
+      <button
+        onClick={() => {
+          setSelectedCategory(null);
+        }}
+        className={!selectedCategory ? styles.active : styles.item}
+      >
+        All
+      </button>
       {categories.map((category) => {
+        <button
+          onClick={() => {
+            setSelectedCategory(category);
+          }}
+          className={
+            selectedCategory === category ? styles.active : styles.item
+          }
+        >
+          All
+        </button>;
         return (
           <button
             onClick={() => {
